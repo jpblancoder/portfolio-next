8a8eb4d990fa1e5ea60fed26b719d172b1a7771f
JP Blanchette <empirecoder@gmail.com>
2018-11-04 21:02:52 -0500

    Fonts and LESS working

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import "../styles/bootstrap/less/bootstrap.less";
 import "../styles/app.less";
+
 import Head from "next/head";
 
 const Index = () => (
@@ -7,6 +9,10 @@ const Index = () => (
       <title>Jean Pierre Blanchette - Portfolio</title>
     </Head>
     <p>Hello Next.js</p>
+    <h1 className="sidebar-title">Jean Pierre Blanchette</h1>
+    <a className="btn btn-primary" href="#/">
+      <i className="fa fa-lg fa-android" aria-hidden="true" /> <strong>About</strong>
+    </a>
   </div>
 );
 
