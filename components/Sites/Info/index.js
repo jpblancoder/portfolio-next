97b9d647f586751c27938b16ae505a2d99200827
JP Blanchette <empirecoder@gmail.com>
2018-11-05 22:41:08 -0500

    Working on Info

--- components/Sites/Info/index.js
+++ components/Sites/Info/index.js
@@ -1,56 +1,89 @@
-const Info = () => (
-  <div className="project-wrapper">
-    <div ng-if="vm.selectedSite.tech.length > 0">
-      <h3 className="project-subtitle">
-        <i className="fa fa-cog" /> Technology
-      </h3>
-      <ul className="project-tech">
-        <li ng-repeat="t in vm.selectedSite.tech">
-          <span ng-bind="t" />
-        </li>
-      </ul>
-    </div>
+import React from "react";
 
-    <div ng-if="vm.selectedSite.features.length > 0">
-      <h3 className="project-subtitle">
-        <i className="fa fa-check-circle" /> Features
-      </h3>
-      <ul className="project-tech">
-        <li ng-repeat="f in vm.selectedSite.features">
-          <span ng-bind="f" />
-        </li>
-      </ul>
-    </div>
+class Info extends React.Component {
+  render() {
+    const { tech, features, category } = this.props.site;
 
-    <div ng-if="vm.selectedSite.category">
-      <h3 className="project-subtitle">
-        <i className="fa fa-copyright" /> Copyright
-      </h3>
-      <div className="project-copyright">
-        <div ng-if="vm.selectedSite.category === 'morneau'">
-          Site Design &amp; Programming:{" "}
-          <a href="http://www.morneaushepell.com" target="_blank" rel="noopener noreferrer">
-            Morneau Shepell
-          </a>
-          .
-        </div>
-        <div ng-if="vm.selectedSite.category === 'shepell'">
-          Site Design &amp; Programming:{" "}
-          <a href="http://www.shepellfgi.com" target="_blank" rel="noopener noreferrer">
-            Shepell&middot;fgi
-          </a>
-          .
-        </div>
-        <div ng-if="vm.selectedSite.category === 'quidnovis'">
-          Site Design &amp; Programming:{" "}
-          <a href="http://www.quidnovis.net/" target="_blank" rel="noopener noreferrer">
-            Quid Novis Internet Productions
-          </a>
-          .
-        </div>
+    return (
+      <div className="project-wrapper">
+        {this.renderTech(tech)}
+        {this.renderFeatures(features)}
+        {this.renderCategory(category)}
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+  renderTech = tech => {
+    return tech.length === 0 ? null : (
+      <div>
+        <h3 className="project-subtitle">
+          <i className="fa fa-cog" /> Technology
+        </h3>
+        <ul className="project-tech">
+          {Object.keys(tech).map((t, i) => (
+            <li key={i}>{t}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  renderFeatures = features => {
+    return features.length === 0 ? null : (
+      <div>
+        <h3 className="project-subtitle">
+          <i className="fa fa-check-circle" /> Features
+        </h3>
+        <ul className="project-tech">
+          {Object.keys(features).map((f, i) => (
+            <li key={i}>{f}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  renderCategory = category => {
+    return (
+      <div>
+        <h3 className="project-subtitle">
+          <i className="fa fa-copyright" /> Copyright
+        </h3>
+        <div className="project-copyright">{this.renderCopyRight(category)}</div>
+      </div>
+    );
+  };
+  renderCopyRight = category => {
+    switch (category) {
+      case "morneau":
+        return (
+          <div>
+            Site Design &amp; Programming:{" "}
+            <a href="http://www.morneaushepell.com" target="_blank" rel="noopener noreferrer">
+              Morneau Shepell
+            </a>
+            .
+          </div>
+        );
+      case "shepell":
+        return (
+          <div>
+            Site Design &amp; Programming:{" "}
+            <a href="http://www.shepellfgi.com" target="_blank" rel="noopener noreferrer">
+              Shepell&middot;fgi
+            </a>
+            .
+          </div>
+        );
+      case "quidnovis":
+        return (
+          <div>
+            Site Design &amp; Programming:{" "}
+            <a href="http://www.quidnovis.net/" target="_blank" rel="noopener noreferrer">
+              Quid Novis Internet Productions
+            </a>
+            .
+          </div>
+        );
+    }
+  };
+}
 
 export default Info;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -9,12 +9,14 @@ import Layout from "../components/Layout";
 import Sidebar from "../components/Sidebar";
 import SiteNav from "../components/Sites/Nav";
 // import SiteShots from "../components/Sites/Shots";
-// import Info from "../components/Sites/Info";
+import Info from "../components/Sites/Info";
 
 import projects from "../lib/projects.js";
 import groupBy from "lodash/groupBy";
 
 const groups = groupBy(projects, "category");
+console.log("projects", projects);
+console.log("groups", groups);
 
 class Portfolio extends React.Component {
   handleCategorySelect = category => {
@@ -45,7 +47,11 @@ class Portfolio extends React.Component {
             </Sidebar>
           </div>
           <div className="col-md-5">{/* <SiteShots site={router.query.site} /> */}</div>
-          <div className="col-md-3">{/* <Info site={router.query.site} /> */}</div>
+          <div className="col-md-3">
+            {JSON.stringify(router.query.category)}
+            {JSON.stringify(router.query.site)}
+            {JSON.stringify(projects[router.query.category])}
+          </div>
         </div>
       </Layout>
     );
