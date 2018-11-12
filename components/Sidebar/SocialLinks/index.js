67d3e667b0034b0455217c9aef8b209fe8d182b2
JP Blanchette <empirecoder@gmail.com>
2018-11-11 23:16:45 -0500

    Better size for social icons

--- components/Sidebar/SocialLinks/index.js
+++ components/Sidebar/SocialLinks/index.js
@@ -5,35 +5,35 @@ const SocialLinks = () => (
     <li>
       <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/jpblancoder">
         <span role="img" aria-label="bird" title="Twitter">
-          <FontAwesomeIcon icon={["fab", "twitter-square"]} size="lg" />
+          <FontAwesomeIcon icon={["fab", "twitter-square"]} size="md" />
         </span>
       </a>
     </li>{" "}
     <li>
       <a target="_blank" rel="noopener noreferrer" href="https://ca.linkedin.com/in/jpblanchette">
         <span role="img" aria-label="chain" title="LinkedIn">
-          <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" />
+          <FontAwesomeIcon icon={["fab", "linkedin"]} size="md" />
         </span>
       </a>
     </li>{" "}
     <li>
       <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jpblancoder/">
         <span role="img" aria-label="camera" title="Instagram">
-          <FontAwesomeIcon icon={["fab", "instagram"]} size="lg" />
+          <FontAwesomeIcon icon={["fab", "instagram"]} size="md" />
         </span>
       </a>
     </li>{" "}
     <li>
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/jpblancoder">
         <span role="img" aria-label="octopus" title="GitHub">
-          <FontAwesomeIcon icon={["fab", "github-square"]} size="lg" />
+          <FontAwesomeIcon icon={["fab", "github-square"]} size="md" />
         </span>
       </a>
     </li>{" "}
     <li>
       <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@jpblancoder">
         <span role="img" aria-label="octopus" title="Medium">
-          <FontAwesomeIcon icon={["fab", "medium"]} size="lg" />
+          <FontAwesomeIcon icon={["fab", "medium"]} size="md" />
         </span>
       </a>
     </li>
